e8208fd791b588dcda19342a0d50781001a29140
Soumyadeep Roy <roys51688@gmail.com>
2026-06-12 21:45:53 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 '''from openai import OpenAI
 import os
 client = OpenAI(
-    api_key="(api_key=os.environ.get(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")
+    api_key="(api_key=os.environ.get(YOUR API KEY")
 
 
 completion = client.chat.completions.create(
